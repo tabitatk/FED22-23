e39375bee2357d046d265a4a64cbc8d4e1ab6543
tabitatk <95043139+tabitatk@users.noreply.github.com>
2022-09-29 14:10:41 +0200

    Add files via upload

--- FED22-23/scripts/script.js
+++ FED22-23/scripts/script.js
@@ -6,8 +6,7 @@ console.log("hi");
 var menu = document.querySelector("header section:nth-of-type(1) button");
 var hamburgerDicht = document.querySelector("header section:nth-of-type(2) button");
 var menuItems = document.querySelector("header section:nth-of-type(2)");
-
-
+var darkmode = document.querySelector(".btn-toggle");
 
 
 
@@ -19,7 +18,10 @@ function verstopHamburger() {
     menuItems.classList.remove("hamburgermenu");
 }
 
-
+// darkmode.addEventListener("click", function darkmode() {
+//     (document.body.classList.toggle("dark-theme"))
+//     document.querySelector(".btn-toggle").textContent = "Lightmode";
+// });
 
 
 
