b0d25338e21baa9f90284f04475250f1bc5d9f67
tabitatk <95043139+tabitatk@users.noreply.github.com>
2022-09-30 15:34:31 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,31 @@
 // JavaScript Document
 console.log("hi");
+
+
+
+var menu = document.querySelector("header section:nth-of-type(1) button");
+var hamburgerDicht = document.querySelector("header section:nth-of-type(2) button");
+var menuItems = document.querySelector("header section:nth-of-type(2)");
+// var vrachtwagen = document.querySelector(" main section:nth-of-type(1) audio");
+
+
+
+function toonMenu() {
+    menuItems.classList.add("hamburgermenu");
+}
+
+function verstopHamburger() {
+    menuItems.classList.remove("hamburgermenu");
+}
+
+// function toeteren() {
+//     var vrachtwagenGeluid = document.querySelector("audio");
+//     vrachtwagenGeluid.play();
+
+// }
+
+
+
+menu.addEventListener("click", toonMenu);
+// vrachtwagen.addEventListener("click", toeteren);
+hamburgerDicht.addEventListener("click", verstopHamburger);
